8693ad09b7bf56b592c8ecce2d3ca390a9d5bb06
Anthony Solis <asolis7378@uta.edu.ec>
2024-04-29 21:49:24 -0500

    ARREGLOS PRESTAMOS CONTROLES

--- src/app/prestamos/prestamos.component.ts
+++ src/app/prestamos/prestamos.component.ts
@@ -21,6 +21,8 @@ export class PrestamosComponent implements OnInit {
   prestamos: Prestamo[] = [];
   selectedPrestamoId: number | undefined;
 
+  idBanco: number = 0;
+
   montoSolicitado: number | undefined;
   plazoMeses: number | undefined;
   interesAnual: number | undefined;
@@ -31,14 +33,25 @@ export class PrestamosComponent implements OnInit {
 
   cobrosIndirectos: any[] = [];
 
+  //Prestamo
+  id_Prestamo: number = 0;
+  tipo: string = '';
+  monto_min: string = '';
+  monto_max: string = '';
+  tasa: string = '';
+  detalle: string = '';
+
+
   constructor(
     private bancoService: BancoService,
     private prestamoService: PrestamoService,
     private cobrosIndirectosService: CobrosIndirectosService,
     private toastr: ToastrService,
-  ) {}
+  ) { }
 
   ngOnInit() {
+    this.idBanco = Number(localStorage.getItem('id_Banco'));
+
     this.getBancos();
   }
 
@@ -54,24 +67,24 @@ export class PrestamosComponent implements OnInit {
     this.selectedBancoId = selectedValue ? parseInt(selectedValue, 10) : undefined;
     this.getPrestamosByBancoId();  // Llamar a getPrestamosByBancoId() al seleccionar un banco
   }
-  
+
 
   getPrestamosByBancoId(): void {
     if (this.selectedBancoId !== undefined) {
       this.prestamoService.getPrestamosByBancoId(this.selectedBancoId)
         .subscribe(prestamos => {
           this.prestamos = prestamos;
-  
+
           // Obtener los cobros indirectos del servicio
           this.cobrosIndirectosService?.getCobrosIndirectosByBancoId(this.selectedBancoId!)
             .subscribe(cobrosIndirectos => {
               // Asignar los cobros indirectos a la propiedad cobrosIndirectos
               this.cobrosIndirectos = cobrosIndirectos;
-  
+
               // Llamar al método submitForm() después de recibir los cobros indirectos
               this.submitForm();
             });
-  
+
           // Establecer el interés anual del primer préstamo como valor predeterminado
           if (this.prestamos.length > 0) {
             this.interesAnual = parseFloat(this.prestamos[0].tasa_interes);
@@ -83,9 +96,9 @@ export class PrestamosComponent implements OnInit {
       console.log('No se ha seleccionado ningún banco.');
     }
   }
-  
-  
-  
+
+
+
 
   selectedBancoLogoUrl(): string | undefined {
     const selectedBanco = this.bancos.find(banco => banco.id_Banco === this.selectedBancoId);
@@ -106,7 +119,7 @@ export class PrestamosComponent implements OnInit {
   onPrestamoSelect(event: Event) {
     const selectedValue = (event.target as HTMLSelectElement).value;
     this.selectedPrestamoId = selectedValue ? parseInt(selectedValue, 10) : undefined;
-  
+
     if (this.selectedPrestamoId) {
       this.prestamoService.getPrestamosByPrestamoId(this.selectedPrestamoId)
         .subscribe(prestamos => {
@@ -120,7 +133,7 @@ export class PrestamosComponent implements OnInit {
         });
     }
   }
-  
+
 
   onTipoAmortizacionSelect(event: Event) {
     const selectedValue = (event.target as HTMLSelectElement).value;
@@ -162,5 +175,39 @@ export class PrestamosComponent implements OnInit {
       // Aquí puedes continuar con tus cálculos
     }
   }
+
+  mostrarRango() {
+    this.prestamoService.getPrestamoByClienteId(this.idBanco).subscribe(
+      (prestamo: Prestamo) => {
+        const monto_min = prestamo.monto_min;
+        const monto_max = prestamo.monto_max;
+        this.toastr.info(`El rango de Monto Solicitado es de: ${monto_min} a ${monto_max}`, 'Rango de préstamo');
+      },
+      (error) => {
+        this.toastr.error('Ocurrió un error al obtener la información de préstamos', 'Error');
+      }
+    );
+  }
   
+  validarPlazo() {
+    const maxMeses = 120;
+    const maxAnios = 10;
+    
+    // Verificar si this.plazoMeses tiene un valor definido
+    if (this.plazoMeses !== undefined) {
+      if (this.plazoMeses > maxMeses) {
+        this.plazoMeses = maxMeses;
+        this.toastr.warning('Solo se permiten un máximo de 120 meses (10 años).');
+      }
+      
+      const mesesEnAnio = 12;
+      if (this.plazoMeses / mesesEnAnio > maxAnios) {
+        this.plazoMeses = maxAnios * mesesEnAnio;
+        this.toastr.warning('Solo se permiten un máximo de 10 años (120 meses).');
+      }
+    }
+  }
+  
+  
+
 }
